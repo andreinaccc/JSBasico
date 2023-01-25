550ad842a5b991b1ae0c5f720095e556f7bcbae5
Andreinaccc <cabezaandreina@gmail.com>
2023-01-25 16:07:05 -0600

    Entrega Tema 2

---
+++ Tema2/Tema2Entrega.js
@@ -0,0 +1,13 @@
+const lista = [
+    "Andreina",
+    35,
+    true,
+    new Date (1987, 11, 13),
+    librofavorito = {
+        titulo: "Los Hombres que no amaban a las mujeres",
+        autor: "Stieg Larson",
+        fecha: new Date (2009, 01, 01),
+        url: "https://es.wikipedia.org/wiki/Los_hombres_que_no_amaban_a_las_mujeres"
+    }
+];
+
