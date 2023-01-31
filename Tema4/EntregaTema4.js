99bc4a70200cd597850be25adb13d3f5818c7afd
Andreinaccc <cabezaandreina@gmail.com>
2023-01-30 22:46:48 -0600

    Entrega 4

---
+++ Tema4/EntregaTema4.js
@@ -0,0 +1,10 @@
+let nombre = "Andreina"
+let apellido = "Cabeza"
+let estudiante = nombre + " " + apellido
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let num = estudiante.length
+let primeraletra = nombre[0]
+let ultimaletra = apellido[apellido.length - 1]
+let sinespacios = estudiante.trim()
+let contiene = estudiante.includes(nombre)
