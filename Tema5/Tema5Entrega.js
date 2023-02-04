95d3416be9951275110ae8017c94cbec8ef74fd8
Andreinaccc <cabezaandreina@gmail.com>
2023-02-03 20:15:44 -0600

    Entrega tema 5

---
+++ Tema5/Tema5Entrega.js
@@ -0,0 +1,7 @@
+let altura = 165;
+let altura_metros = 1.65;
+let peso = 60.5;
+let altura_max = Math.ceil(altura_metros);
+let peso_min = Math.floor (peso);
+let igual = Number.MAX_VALUE === Number.MAX_VALUE + 1;
+
