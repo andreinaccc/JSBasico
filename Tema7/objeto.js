c33d6eea09752daedda480063ab9c8285581530f
Andreinaccc <cabezaandreina@gmail.com>
2023-02-04 18:47:57 -0600

    Ejercicio 2 Tema 7

---
+++ Tema7/objeto.js
@@ -0,0 +1,28 @@
+datosPersonales = {
+    nombre: "Andreina",
+    apellido: "Cabeza",
+    edad: 35,
+    altura: "165cm",
+    eresDesarrollador: true
+}
+
+let edad = datosPersonales.edad
+
+const lista = [{...datosPersonales}, 
+    {
+        nombre: "Angelis",
+        apellido: "Centofanti",
+        edad: 31,
+        altura: "160cm",
+        eresDesarrollador: false
+    }, 
+    {   nombre: "Marian",
+        apellido: "Cabello",
+        edad: 38,
+        altura: "150cm",
+        eresDesarrollador: false
+    }]
+
+    lista.sort((a,b)=>a.edad - b.edad)
+
+    
